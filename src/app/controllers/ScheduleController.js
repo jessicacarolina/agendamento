48fbf61c0d05ad88a7266166c4fd565494fdb77c
Jéssica Carolina Silva <jessica.carolina.silva26@gmail.com>
2020-06-29 21:31:31 -0300

    Sistema de agendamento

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,40 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    // vou pegar todos os agendamentos iniciados em:
+    // 2020-01-08 00:00:00
+    // e terminados em :
+    // 2020-01-08 23:59:59
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
